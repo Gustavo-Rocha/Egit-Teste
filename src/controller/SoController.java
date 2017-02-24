44d7279ba12929060491e0852e11342b4752b713
Gustavo-Rocha <gustavooliveirarocha@hotmail.com>
2017-02-24 14:46:04 -0300

    Primeiro projeto  do EGit para o github

---
+++ src/controller/SoController.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class SoController 
+{
+
+	public SoController()
+	{
+		super();
+		
+	}
+	
+	public String os()
+	{
+		
+		return System.getProperty("os.name");
+		
+	}
+	
+}
